f6efc1453083fc98d90ba20f4a2219a924104c0b
小鱼 <wujy1025@126.com>
2023-01-16 10:26:38 +0800

    feat: 增加rollup打包以及相关配置

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import resolve from "@rollup/plugin-node-resolve";
+import babel from '@rollup/plugin-babel';
+import terser from "@rollup/plugin-terser"; // 不用解构出来 有默认导出
+
+export default {
+  input: "index.js",
+  output: [
+    {
+      file: "dist/bundle.cjs.js",
+      format: "cjs",
+    },
+    {
+      file: "dist/bundle.esm.js",
+      format: "es",
+    },
+    {
+      file: "dist/bundle.min.js",
+      format: "iife",
+      name: "version",
+    },
+  ],
+  external: [],
+  plugins: [
+    resolve({
+      // 将自定义选项传递给解析插件
+      customResolveOptions: {
+        moduleDirectories: ["node_modules"],
+      },
+    }),
+    babel({ babelHelpers: 'bundled' }),
+    terser(),
+  ],
+};
