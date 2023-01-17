37227d344a2c34e564af8db581e4a608498c1c2d
langjiacheng <yunlong@yscredit.com>
2023-01-17 17:06:37 +0800

    fix: 代码格式化

--- src/tools/toCamelFormat.js
+++ src/tools/toCamelFormat.js
@@ -1,12 +1,12 @@
 /**
- * 
- * @param {str} 非驼峰字符串 
- * @param {re} 间隔符 
+ *
+ * @param {str} 非驼峰字符串
+ * @param {re} 间隔符
  * @returns 驼峰字符串
  */
-export function toCamelFormat(str, re = "_") {
+export function toCamelFormat(str, re = '_') {
   if (!str) {
-    console.error("toCamel参数错误");
+    console.error('toCamel参数错误');
     return;
   }
 
@@ -17,5 +17,5 @@ export function toCamelFormat(str, re = "_") {
       if (i == 0) return v;
       return v[0].toUpperCase() + v.slice(1);
     })
-    .join("");
+    .join('');
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -6,42 +6,42 @@ import {
   identityFormat,
   emailFormat,
   nameFormat,
-  toCamelFormat,
-} from "../src/index.js";
+  toCamelFormat
+} from '../src/index.js';
 
-describe("测试", () => {
-  it("测试dateFormat", () => {
-    expect(dateFormat("2022/01/02")).toBe("2022-01-02");
+describe('测试', () => {
+  it('测试dateFormat', () => {
+    expect(dateFormat('2022/01/02')).toBe('2022-01-02');
   });
 
-  it("测试moneyFormat", () => {
-    expect(moneyFormat("10000")).toBe("10,000.00");
+  it('测试moneyFormat', () => {
+    expect(moneyFormat('10000')).toBe('10,000.00');
   });
 
-  it("测试numberFormat", () => {
-    expect(numberFormat("10000")).toBe("10,000");
+  it('测试numberFormat', () => {
+    expect(numberFormat('10000')).toBe('10,000');
   });
 
-  it("测试phoneFormat", () => {
-    expect(phoneFormat("17612341234")).toBe("176****1234");
+  it('测试phoneFormat', () => {
+    expect(phoneFormat('17612341234')).toBe('176****1234');
   });
 
-  it("测试identityFormat", () => {
-    expect(identityFormat("330921199110253510")).toBe("330***********3510");
+  it('测试identityFormat', () => {
+    expect(identityFormat('330921199110253510')).toBe('330***********3510');
   });
 
-  it("测试emailFormat", () => {
-    expect(emailFormat("1234abcdefg@163.com")).toBe("123****@163.com");
+  it('测试emailFormat', () => {
+    expect(emailFormat('1234abcdefg@163.com')).toBe('123****@163.com');
   });
 
-  it("测试nameFormat", () => {
-    expect(nameFormat("王丫丫", 1)).toBe("王**");
-    expect(nameFormat("王丫丫", 2, 1)).toBe("*丫丫");
+  it('测试nameFormat', () => {
+    expect(nameFormat('王丫丫', 1)).toBe('王**');
+    expect(nameFormat('王丫丫', 2, 1)).toBe('*丫丫');
   });
 
-  it("测试toCamelFormat", () => {
-    expect(toCamelFormat("A_B_C")).toBe("ABC");
-    expect(toCamelFormat("a_b_c")).toBe("aBC");
-    expect(toCamelFormat("a隔b隔c", "隔")).toBe("aBC");
+  it('测试toCamelFormat', () => {
+    expect(toCamelFormat('A_B_C')).toBe('ABC');
+    expect(toCamelFormat('a_b_c')).toBe('aBC');
+    expect(toCamelFormat('a隔b隔c', '隔')).toBe('aBC');
   });
 });
